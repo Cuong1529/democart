d1a99a822f1a9cfb079c3000a9a28a013e41c8ea
Brucelee <conthuong151295@gmail.com>
2022-06-19 23:40:44 +0700

    case : useEffect with clean up

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -5,15 +5,34 @@ import DUMMY_MEALS from "./constant";
 import classes from './AvailableMeals.module.css'
 
 function AvailableMeals() {
-
+  const [isShowGoToTop, setIsShowGoToTop] = useState(false);
   const [meals, setMeals] = useState([])
 
-  useEffect( () => {
-     Promise.resolve(DUMMY_MEALS).then((meals) => {
+  useEffect(() => {
+    Promise.resolve(DUMMY_MEALS).then((meals) => {
       setMeals(meals)
     });
   }, [])
 
+  useEffect(() => {
+    console.log('useEffect...');
+    const handleScroll = () => {
+      if (window.scrollY >= 200) {
+        setIsShowGoToTop(true)
+      } else {
+        setIsShowGoToTop(false)
+      }
+    }
+    window.addEventListener('scroll', handleScroll)
+    return () => {
+      console.log('component will unmount');
+      window.removeEventListener('scroll', handleScroll)
+    }
+  },[])
+
+  const goToTopHandler = () => {
+    window.scrollTo(0, 0);
+  }
   const availableMeals = meals.map((meal, index) => {
     return (
       <MealItem
@@ -31,6 +50,14 @@ function AvailableMeals() {
       <Card>
         <ul>{availableMeals}</ul>
       </Card>
+      {isShowGoToTop && <button onClick={goToTopHandler}
+        style={{
+          position: 'fixed',
+          right: 0,
+          bottom: 20
+        }
+        }>Go to top
+      </button>}
     </section>
   );
 }
@@ -47,61 +74,61 @@ function AvailableMeals() {
 //     this.handleResetMeals = this.handleResetMeals.bind(this);
 //   }
 
-  // async componentDidMount() {
-  // await Promise.resolve(DUMMY_MEALS).then((meals) => {
-  //   this.setState({
-  //     mealList: meals,
-  //   });
-  // });
+// async componentDidMount() {
+// await Promise.resolve(DUMMY_MEALS).then((meals) => {
+//   this.setState({
+//     mealList: meals,
+//   });
+// });
 
-  //     timer = setTimeout(() => alert("Get mealList success"), 3000);
+//     timer = setTimeout(() => alert("Get mealList success"), 3000);
 
-  //   }
+//   }
 
-  //   componentWillUnmount() {
-  //     console.log("Available meals unmount");
-  //     clearTimeout(timer);
-  //   }
+//   componentWillUnmount() {
+//     console.log("Available meals unmount");
+//     clearTimeout(timer);
+//   }
 
-  //   handleResetMeals() {
-  //     this.setState({
-  //       isResetMeal: !this.state.isResetMeal,
-  //     });
-  //   }
+//   handleResetMeals() {
+//     this.setState({
+//       isResetMeal: !this.state.isResetMeal,
+//     });
+//   }
 
-  //   render() {
-  //     console.log("re-render");
-  //     const meals = this.state.mealList.map((meal, index) => {
-  //       return (
-  //         <MealItem
-  //           id={meal.id}
-  //           key={meal.id}
-  //           name={meal.name}
-  //           description={meal.description}
-  //           price={meal.price}
-  //           isResetMeal={this.state.isResetMeal}
-  //           handleResetMeals={this.handleResetMeals}
-  //         />
-  //       );
-  //     });
+//   render() {
+//     console.log("re-render");
+//     const meals = this.state.mealList.map((meal, index) => {
+//       return (
+//         <MealItem
+//           id={meal.id}
+//           key={meal.id}
+//           name={meal.name}
+//           description={meal.description}
+//           price={meal.price}
+//           isResetMeal={this.state.isResetMeal}
+//           handleResetMeals={this.handleResetMeals}
+//         />
+//       );
+//     });
 
-  //     return (
-  //       <AvailableMealsWrapper>
-  //         <section className="meals">
-  //           <button
-  //             type="reset"
-  //             onClick={this.handleResetMeals}
-  //             className="btnReset"
-  //           >
-  //             Reset All
-  //           </button>
-  //           <Card>
-  //             <ul>{meals}</ul>
-  //           </Card>
-  //         </section>
-  //       </AvailableMealsWrapper>
-  //     );
-  //   }
-  // }
+//     return (
+//       <AvailableMealsWrapper>
+//         <section className="meals">
+//           <button
+//             type="reset"
+//             onClick={this.handleResetMeals}
+//             className="btnReset"
+//           >
+//             Reset All
+//           </button>
+//           <Card>
+//             <ul>{meals}</ul>
+//           </Card>
+//         </section>
+//       </AvailableMealsWrapper>
+//     );
+//   }
+// }
 
-  export default AvailableMeals;
+export default AvailableMeals;
